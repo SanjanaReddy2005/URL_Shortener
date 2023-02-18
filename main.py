3b6b2c51403fe68bb1210cc4784952f2f5abb79e
SanjanaReddy2005 <117728257+SanjanaReddy2005@users.noreply.github.com>
2023-02-18 20:07:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+def shorten_link(full_link,link_name):
+     API_KEY = 'a875b8af1135cdaf8eaea992f77ef3af9ac32'
+     # BASE_URL = ''
+     BASE_URL = 'https://cutt.ly/api/api.php'
+
+     payload = {'key': API_KEY, 'short': full_link, 'name':link_name}
+     request = requests.get(BASE_URL, params=payload)
+     data = request.json()
+
+     print(data)
+
+
+shorten_link(input('Enter the url which you want to shorten it :'),input('Enter the name which you want to give it :'))
+
+# import pyshorteners
+
+# link = input("enter the link : ")
+
+# shortener = pyshorteners.Shortener()
+
+# x = shortener.tinyurl.short(link)
+
+# print(x)
